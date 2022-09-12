c9d99a25c66af79a445ff7ef816dd462c33d6680
ABBorges1 <adrielbarborges@gmail.com>
2022-09-12 20:29:31 -0300

    Inicio do CRUD

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,15 @@
 require ('dotenv').config();
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
+//conexão ao banco de dados remoto
 const connectDb = require('./config/database')
 connectDb();
 
@@ -20,6 +20,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+/* Rotas da aplicação */
+const userRoute = require('./routes/user');
+app.use('/user', userRoute);
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -25,4 +25,64 @@ controller.create = async(req, res)=>{
     }
 }
 
+controller.retrieveAll = async(req, res)=>{
+    try{
+        //find() sem parâmetros retorna todos os documentos da coleção
+        const result = await User.find()
+        //HTTP 200: OK (implícito)
+        res.send(result)
+    }
+    catch(error){
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
+}
+
+controller.retrieveOne = async (req, res)=>{
+    try{
+        const result = await User.findById(req.params.id)
+
+        //HTTP 200: OK (implícito)
+        if(result)res.send(result)  // Encontrou o documento
+        //HTTP 404: Not Found
+        else res.status(404).end()  // Não encontrou
+    }
+    catch(error){
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
+}
+
+controller.update = async(req, res)=>{
+    try{
+        const result = await User.findByIdAndUpdate(req.params.id, req.body)
+
+        //HTTP 204: No content
+        if(result) return res.status(204).end() // Encontrou e atualizou 
+        else res.status(404).end()              // Não encontrou
+    }
+    catch(error){
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
+}
+
+controller.delete = async(req, res)=>{
+    try{
+        const result = await User.findByIdAndDelete(req.params.id)
+
+        //HTTP 204: No content
+        if(result) return res.status(204).end() // Encontrou e atualizou 
+        else res.status(404).end()              // Não encontrou
+    }
+    catch(error){
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
+}
+
 module.exports = controller
